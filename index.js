15f6f9b2a319b69b2f5852b3454c7e46b8ce74da
Aryan <aryankushwahalifenote@gmail.com>
2024-06-26 13:10:31 +0530

    V1.0.0

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const downloadController = require('./Controllers/downloadController');
 const app = express();
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the YTD');
+});
+
 app.get('/download', downloadController);
 
 app.listen(4000, () => {
