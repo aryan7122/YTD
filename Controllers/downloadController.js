03529f8aaa72cd4caf58d86f4e3fdbc97c69378c
Aryan <aryankushwahalifenote@gmail.com>
2024-06-26 14:00:06 +0530

    V1.0.0

--- Controllers/downloadController.js
+++ Controllers/downloadController.js
@@ -2,16 +2,17 @@ const ytdl = require('ytdl-core');
 
 const downloadController = async (req, res) => {
     try {
-        const url = req.query.url
-        const videoId = await ytdl.getURLVideoID(url)
-        const metaInfo = await ytdl.getInfo(url)
+        const url = req.query.url;
+        const videoId = await ytdl.getURLVideoID(url);
+        const metaInfo = await ytdl.getInfo(url);
         let data = {
             url: 'https://www.youtube.com/embed/' + videoId,
             info: metaInfo.formats
-        }
-        return res.send(data)
+        };
+        return res.send(data);
     } catch (error) {
-        return res.status(500)
+        console.error('Error occurred:', error);
+        return res.status(500).send('Error occurred while downloading');
     }
 };
 
